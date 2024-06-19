9b851ece25786e325afade87e56a5e7e01438e2b
peng.chen2 <>
2024-06-19 16:22:32 +0800

    config: 配置移动端rem

--- packages/nuxt-app/nuxt.config.ts
+++ packages/nuxt-app/nuxt.config.ts
@@ -1,4 +1,18 @@
+import { join } from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: join(__dirname, 'tailwind.config.js'),
+      'postcss-pxtorem': {
+        rootValue: 75, // 1rem的大小
+        propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+        exclude: ['node_modules'],
+      },
+    }
+  }
 })
